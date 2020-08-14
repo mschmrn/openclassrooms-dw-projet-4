7ac142c7ca36d0a9b14335535ad8cc984e4d5d4a
mschmrn <ismael.jouhari@gmail.com>
2020-08-14 18:47:44 +0200

    Tests utilisateurs backend

--- view/backend/sidebar.php
+++ view/backend/sidebar.php
@@ -1,31 +1,31 @@
 <!-- LOGO NAV -->
 <nav class="backend p-0 border-bottom border-grey">
-    <div class="d-flex align-items-center justify-content-start ml-4 ml-md-3 logonav  ">
+    <div class="d-flex align-items-center justify-content-start ml-4 ml-md-3 ml-xl-4 logonav  ">
         <a href="index.php?controller=admin&task=index"><img src="../public/images/logo.svg" class="" alt=""></a>
     </div>
 </nav>
 <!-- MENU -->
 <div class="menu my-5 px-2">
-    <a class="nav-link item py-2<?php if ($path != "index") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=index">
-        <img class="<?php if ($path === "index") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/apps.svg" alt="Dashboard"><span class="hide">Dashboard</span>
+    <a class="nav-link item py-2" href="index.php?controller=admin&task=index">
+        <img class="<?php if ($path === "index") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/apps.svg" alt="Dashboard"><span class="side-text hide<?php if ($path != "index") { ?> text-white color-hover<?php } ?>">Dashboard</span>
     </a>
-    <a class="nav-link item py-2<?php if ($path != "articles/edit") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=editArticle">
-        <img class="<?php if ($path === "articles/edit") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/create.svg" alt="Dashboard"><span class="hide">Écrire un article</span>
+    <a class="nav-link item py-2" href="index.php?controller=admin&task=editArticle">
+        <img class="<?php if ($path === "articles/edit") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/create.svg" alt="Dashboard"><span class="hide <?php if ($path != "articles/edit") { ?> text-white color-hover<?php } ?>">Écrire un article</span>
     </a>
     <a class="nav-link item py-2<?php if ($path != "articles/index") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=viewArticles">
-        <img class="<?php if ($path === "articles/index") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/articles.svg" alt="Dashboard"><span class="hide">Articles</span>
+        <img class="<?php if ($path === "articles/index") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/articles.svg" alt="Dashboard"><span class="hide">Articles</span>
     </a>
     <a class="nav-link item py-2<?php if ($path != "articles/drafts") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=viewDrafts">
-        <img class="<?php if ($path === "articles/drafts") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/drafts.svg" alt="Dashboard"><span class="hide">Brouillons</span>
+        <img class="<?php if ($path === "articles/drafts") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/drafts.svg" alt="Dashboard"><span class="hide">Brouillons</span>
     </a>
     <a class="nav-link item py-2<?php if ($path != "comments") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=viewComments">
-        <img class="<?php if ($path === "comments") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/comments.svg" alt="Dashboard"><span class="hide">Commentaires</span>
+        <img class="<?php if ($path === "comments") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/comments.svg" alt="Dashboard"><span class="hide">Commentaires</span>
     </a>
     <a class="nav-link item py-2<?php if ($path != "users") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=viewUsers">
-        <img class="<?php if ($path === "users") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/users.svg" alt="Dashboard"><span class="hide">Utilisateurs</span>
+        <img class="<?php if ($path === "users") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/users.svg" alt="Dashboard"><span class="hide">Utilisateurs</span>
     </a>
     <a class="nav-link item py-2<?php if ($path != "trash") { ?> text-white color-hover<?php } ?>" href="index.php?controller=admin&task=viewTrash">
-        <img class="<?php if ($path === "trash") { ?> filter-test <?php } else { ?> invert-colors <?php } ?>" src="../public/images/trash.svg" alt="Dashboard"><span class="hide">Corbeille</span>
+        <img class="<?php if ($path === "trash") { ?> svg-red <?php } else { ?> invert-colors <?php } ?>" src="../public/images/trash.svg" alt="Dashboard"><span class="hide">Corbeille</span>
     </a>
 
     <span class="hide">
